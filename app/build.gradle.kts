db69cfed704fe4bb0e0b1cdc354c30f771a7f59e
Denisa <denisa.popescu1905@gmail.com>
2025-03-10 21:28:44 +0200

    Testing Client connection to physical Android device

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.ss_final_java"
-        minSdk = 34
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -32,14 +32,17 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
     implementation("org.eclipse.paho:org.eclipse.paho.android.service:1.1.1")
+
+    implementation ("androidx.localbroadcastmanager:localbroadcastmanager:1.1.0")
+
+
 
 }
--- app/src/main/java/com/example/ss_final_java/MainActivity.java
+++ app/src/main/java/com/example/ss_final_java/MainActivity.java
@@ -1,117 +1,83 @@
 package com.example.ss_final_java;
+
 import android.os.Bundle;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
-
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.*;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.security.KeyStore;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateFactory;
-
-import javax.net.ssl.*;
+import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MQTTClient";
-    private MqttAndroidClient mqttClient;
+    private static final String TAG = "MQTT"; // Tag for Logcat
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "START");
         setContentView(R.layout.activity_main);
 
-        String brokerUrl = "ssl://mqtt-broker-address:8883"; // Configurable
-        String clientId = "androidClient";
+        //String broker = "tcp://192.168.42.19:1883";
+        String broker = "tcp://10.41.202.136:1883";
 
-        mqttClient = new MqttAndroidClient(getApplicationContext(), brokerUrl, clientId);
+
+        String clientId = "demo_client";
+        String topic = "topic/test";
+        int subQos = 1;
+        int pubQos = 1;
+        String msg = "Hello MQTT";
 
         try {
+            // Initialize MQTT client with in-memory persistence
+            MqttClient client = new MqttClient(broker, clientId, new MqttDefaultFilePersistence(getFilesDir().getAbsolutePath()));
             MqttConnectOptions options = new MqttConnectOptions();
-            options.setUserName("admin");
-            options.setPassword("123456".toCharArray());
-            options.setSocketFactory(createSSLSocketFactory());
+            options.setCleanSession(true);  // Optionally use clean session
+            Log.d(TAG, "Connecting to broker: " + broker);
+            client.connect(options);
+            Log.d(TAG, "Connected to broker");
 
+            if (client.isConnected()) {
+                // Set callback to handle incoming messages
+                client.setCallback(new MqttCallback() {
 
-            mqttClient.connect(options, null, new IMqttActionListener() {
-                @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
-                    Log.d(TAG, "Conectare reușită la broker!");
-                    subscribeToTopic();
-                    publishMessage("Hello from Android!");
-                }
+                    // When a message arrives
+                    public void messageArrived(String topic, MqttMessage message) throws Exception {
+                        Log.d(TAG, "Message arrived on topic: " + topic);
+                        Log.d(TAG, "QoS: " + message.getQos());
+                        Log.d(TAG, "Message content: " + new String(message.getPayload()));
+                    }
 
-                @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    Log.e(TAG, "Conectare eșuată: " + exception.getMessage());
-                }
-            });
+                    // When the connection is lost
+                    public void connectionLost(Throwable cause) {
+                        Log.e(TAG, "Connection lost: " + cause.getMessage());
+                    }
 
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
-    }
+                    // When message delivery is complete
+                    public void deliveryComplete(IMqttDeliveryToken token) {
+                        Log.d(TAG, "Delivery complete: " + token.isComplete());
+                    }
+                });
 
-    private SSLSocketFactory createSSLSocketFactory() {
-        try {
-            BufferedInputStream caInput = new BufferedInputStream(Files.newInputStream(Paths.get("path")));
-            KeyStore keyStore = KeyStore.getInstance("PKCS12");
-            keyStore.load(Files.newInputStream(Paths.get("path")), "password".toCharArray()); // Client certificate
+                // Subscribe to the topic
+                Log.d(TAG, "Subscribing to topic: " + topic);
+                client.subscribe(topic, subQos);
 
-            CertificateFactory cf = CertificateFactory.getInstance("X.509");
-            Certificate ca = cf.generateCertificate(caInput);
-            keyStore.setCertificateEntry("ca", ca);
-
-            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-            keyManagerFactory.init(keyStore, "clientpassword".toCharArray());
-            KeyManager[] keyManagers = keyManagerFactory.getKeyManagers();
-
-            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-            trustManagerFactory.init((KeyStore) null);
-            TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
-
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(keyManagers, trustManagers, new java.security.SecureRandom());
-            return sslContext.getSocketFactory();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private void subscribeToTopic() {
-        try {
-            mqttClient.subscribe("topic/example", 0);
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void publishMessage(String message) {
-        try {
-            MqttMessage mqttMessage = new MqttMessage(message.getBytes());
-            mqttMessage.setQos(0);
-            mqttClient.publish("topic/example", mqttMessage);
-            Log.d(TAG, "Mesaj trimis: " + message);
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (mqttClient != null && mqttClient.isConnected()) {
-            try {
-                mqttClient.disconnect();
-                Log.d(TAG, "Deconectare de la broker");
-            } catch (MqttException e) {
-                e.printStackTrace();
+                // Create and publish a message
+                MqttMessage message = new MqttMessage(msg.getBytes());
+                message.setQos(pubQos);
+                Log.d(TAG, "Publishing message: " + msg);
+                client.publish(topic, message);
             }
+
+            // Disconnect from the broker
+            client.disconnect();
+            Log.d(TAG, "Disconnected from broker");
+
+            client.close();
+            Log.d(TAG, "Client closed");
+
+        } catch (MqttException e) {
+            Log.e(TAG, "Error: " + e.getMessage(), e);
         }
     }
 }
